2795aa068a275ddc1d8ba017d486c58bb228cb73
david@grant.org.uk <david@grant.org.uk@b9633145-1b50-0410-95e4-9528dd1f157e>
2008-06-24 19:18:51 +0000

    Javadoc changes.

--- trunk/src/com/google/code/jsudokusolver/InvalidSudokuException.java
+++ trunk/src/com/google/code/jsudokusolver/InvalidSudokuException.java
@@ -1,8 +1,17 @@
 package com.google.code.jsudokusolver;
 
+/**
+ * This exception is thrown when a grid is filled with an invalid puzzle
+ * 
+ * @author David Grant
+ */
 public class InvalidSudokuException extends Exception {
-    public InvalidSudokuException(String message)
-    {
+    /**
+     * Constructor
+     * 
+     * @param message description of this exception
+     */
+    public InvalidSudokuException(String message) {
         super(message);
     }
 }
