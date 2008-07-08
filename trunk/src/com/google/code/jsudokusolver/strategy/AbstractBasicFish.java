8b4f2a5608d454164636bb7c7246f68c0721bd7c
david@grant.org.uk <david@grant.org.uk@b9633145-1b50-0410-95e4-9528dd1f157e>
2008-07-08 13:10:39 +0000

    Changed fish implementation to using method described here:
    http://www.sudopedia.org/wiki/Fish_and_Subsets#Trawling_for_Fish_Using_Locked_Sets

--- trunk/src/com/google/code/jsudokusolver/strategy/AbstractBasicFish.java
+++ trunk/src/com/google/code/jsudokusolver/strategy/AbstractBasicFish.java
@@ -1,10 +1,11 @@
 package com.google.code.jsudokusolver.strategy;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import com.google.code.jsudokusolver.Cell;
 import com.google.code.jsudokusolver.Grid;
@@ -33,81 +34,63 @@ abstract public class AbstractBasicFish implements SolvingStrategy {
     private boolean solveRow() {
         List<House> rows = grid.getRows();
         for (int i = 1; i <= grid.getSize(); i++) {
-            Set<Cell> fishCells = new HashSet<Cell>();
+            Map<Integer, Set<Integer>> rowMap = new TreeMap<Integer, Set<Integer>>();
+            Set<Integer> columnPopulation = new TreeSet<Integer>();
             for (House row : rows) {
                 Set<Cell> cells = row.getCellsWithCandidate(i);
-                // Filter rows with the wrong number of cells
-                if (cells.size() > getFishSize() || cells.size() < 2) {
-                    continue;
-                }
-                int chute = 0;
-                boolean valid = false;
+                Set<Integer> columnSet = new TreeSet<Integer>();
                 for (Cell cell : cells) {
-                    int cellChute = cell.getColumn().getChute();
-                    if (chute == 0) {
-                        chute = cellChute;
-                    }
-                    if (chute != cellChute) {
-                        // One of the cells is in a different chute,
-                        // so we can proceed.
-                        valid = true;
-                        break;
-                    }
+                    columnSet.add(cell.getColumn().getOffset());
                 }
-                // We can't continue when all the cells are in the same chute.
-                if (valid == false) {
-                    continue;
-                }
-                for (Cell cell : cells) {
-                    fishCells.add(cell);
+                // Ignore singles
+                // We could probably optimise here to remove
+                // rows that can't possibly fit into the combinations below
+                if (columnSet.size() > 1) {
+                    columnPopulation.addAll(columnSet);
+                    rowMap.put(row.getOffset(), columnSet);
                 }
             }
-            
-            Set<Integer> colElements = new HashSet<Integer>(); 
-            for (Cell cell : fishCells) {
-            	colElements.add(cell.getColumn().getOffset());
+            // There needs to be more rows than this fish size, otherwise
+            // there will be no cells to remove the candidate from.
+            if (rowMap.size() <= getFishSize()) {
+                continue;
             }
-            Set<Integer> rowElements = new HashSet<Integer>(); 
-            for (Cell cell : fishCells) {
-            	rowElements.add(cell.getRow().getOffset());
-            }
-            
-            Set<Set<Integer>> colCombinations = new HashSet<Set<Integer>>();
-            colCombinations = Cell.generateCombinations(colElements, colCombinations, getFishSize());
-            Set<Set<Integer>> rowCombinations = new HashSet<Set<Integer>>();
-            rowCombinations = Cell.generateCombinations(rowElements, rowCombinations, getFishSize());
 
-            for (Set<Integer> colCombination : colCombinations) {
-               	for (Set<Integer> rowCombination : rowCombinations) {
-               		Map<Integer, Integer> rowFrequency = getFrequencyMap(rowCombination);
-               		Map<Integer, Integer> colFrequency = getFrequencyMap(colCombination);
-               		Set<Cell> matches = new HashSet<Cell>();
-               		for (Cell cell : fishCells) {
-               			int row = cell.getRow().getOffset();
-            			int col = cell.getColumn().getOffset();
-            			if (rowCombination.contains(row) && colCombination.contains(col)) {
-            				matches.add(cell);
-            				incrementFrequencyMap(rowFrequency, row);
-            				incrementFrequencyMap(colFrequency, col);
-            			}
-               		}
-                	if (validateFrequencyMap(rowFrequency) && validateFrequencyMap(colFrequency)) {
-                		boolean changed = false;
-    	            	for (Cell cell : matches) {
-    	            		House column = cell.getColumn();
-    	            		Set<Cell> colCells = new HashSet<Cell>(column.getCellsWithCandidate(i));
-    	            		colCells.removeAll(matches);
-    	            		for (Cell colCell : colCells) {
-    	            			if (colCell.remove(i)) {
-    	            				changed = true;
-    	                            Grid.logCandidateRemoval(colCell, i, getName() + "-R", matches);
-    	            			}
-    	            		}
-    	            	}
-    	            	if (changed) {
-    	                    return true;
-    	                }
-                	}
+            Set<Set<Integer>> combinations = new TreeSet<Set<Integer>>();
+            combinations = Cell.generateCombinations(columnPopulation, 
+                                                     combinations, 
+                                                     getFishSize());
+            
+            for (Set<Integer> combination : combinations) {
+                Set<Map.Entry<Integer, Set<Integer>>> matches = new HashSet<Map.Entry<Integer, Set<Integer>>>();
+                for (Map.Entry<Integer, Set<Integer>> entry : rowMap.entrySet()) {
+                    if (combination.containsAll(entry.getValue())) {
+                        matches.add(entry);
+                    }
+                }
+                if (matches.size() == getFishSize()) {
+                    Set<Cell> fish = new TreeSet<Cell>();
+                    for (Map.Entry<Integer, Set<Integer>> entry : matches) {
+                        House row = rows.get(entry.getKey() - 1);
+                        for (Integer column : entry.getValue()) {
+                            fish.add(row.getCells().get(column - 1));
+                        }
+                        rowMap.remove(entry.getKey());
+                    }
+                    boolean solved = false;
+                    for (Map.Entry<Integer, Set<Integer>> entry : rowMap.entrySet()) {
+                        entry.getValue().retainAll(combination);
+                        House row = rows.get(entry.getKey() - 1);
+                        for (Integer column : entry.getValue()) {
+                            Cell cell = row.getCells().get(column - 1);
+                            cell.remove(i);
+                            Grid.logCandidateRemoval(cell, i, getName(), fish);
+                            solved = true;
+                        }
+                    }
+                    if (solved) {
+                        return true;
+                    }
                 }
             }
         }
@@ -117,104 +100,66 @@ abstract public class AbstractBasicFish implements SolvingStrategy {
     private boolean solveColumn() {
         List<House> columns = grid.getColumns();
         for (int i = 1; i <= grid.getSize(); i++) {
-            Set<Cell> fishCells = new HashSet<Cell>();
+            Map<Integer, Set<Integer>> columnMap = new TreeMap<Integer, Set<Integer>>();
+            Set<Integer> rowPopulation = new TreeSet<Integer>();
             for (House column : columns) {
                 Set<Cell> cells = column.getCellsWithCandidate(i);
-                // Filter columns with the wrong number of cells
-                if (cells.size() > getFishSize() || cells.size() < 2) {
-                    continue;
-                }
-                int chute = 0;
-                boolean valid = false;
+                Set<Integer> rowSet = new TreeSet<Integer>();
                 for (Cell cell : cells) {
-                    int cellChute = cell.getRow().getChute();
-                    if (chute == 0) {
-                        chute = cellChute;
-                    }
-                    if (chute != cellChute) {
-                        // One of the cells is in a different chute,
-                        // so we can proceed.
-                        valid = true;
-                        break;
-                    }
+                    rowSet.add(cell.getRow().getOffset());
                 }
-                // We can't continue when all the cells are in the same chute.
-                if (valid == false) {
-                    continue;
-                }
-                for (Cell cell : cells) {
-                    fishCells.add(cell);
+                // Ignore singles
+                // We could probably optimise here to remove
+                // rows that can't possibly fit into the combinations below
+                if (rowSet.size() > 1) {
+                    rowPopulation.addAll(rowSet);
+                    columnMap.put(column.getOffset(), rowSet);
                 }
             }
-            Set<Integer> rowElements = new HashSet<Integer>(); 
-            for (Cell cell : fishCells) {
-            	rowElements.add(cell.getRow().getOffset());
+            // There needs to be more rows than this fish size, otherwise
+            // there will be no cells to remove the candidate from.
+            if (columnMap.size() <= getFishSize()) {
+                continue;
             }
-            Set<Integer> colElements = new HashSet<Integer>(); 
-            for (Cell cell : fishCells) {
-            	colElements.add(cell.getColumn().getOffset());
-            }
-            
-            Set<Set<Integer>> rowCombinations = new HashSet<Set<Integer>>();
-            rowCombinations = Cell.generateCombinations(rowElements, rowCombinations, getFishSize());
-            Set<Set<Integer>> colCombinations = new HashSet<Set<Integer>>();
-            colCombinations = Cell.generateCombinations(colElements, colCombinations, getFishSize());
 
-            for (Set<Integer> rowCombination : rowCombinations) {
-               	for (Set<Integer> colCombination : colCombinations) {
-               		Map<Integer, Integer> rowFrequency = getFrequencyMap(rowCombination);
-               		Map<Integer, Integer> colFrequency = getFrequencyMap(colCombination);
-               		Set<Cell> matches = new HashSet<Cell>();
-               		for (Cell cell : fishCells) {
-               			int row = cell.getRow().getOffset();
-            			int col = cell.getColumn().getOffset();
-            			if (rowCombination.contains(row) && colCombination.contains(col)) {
-            				matches.add(cell);
-            				incrementFrequencyMap(rowFrequency, row);
-            				incrementFrequencyMap(colFrequency, col);
-            			}
-               		}
-                	if (validateFrequencyMap(rowFrequency) && validateFrequencyMap(colFrequency)) {
-                		boolean changed = false;
-    	            	for (Cell cell : matches) {
-    	            		House row = cell.getRow();
-    	            		Set<Cell> rowCells = new HashSet<Cell>(row.getCellsWithCandidate(i));
-    	            		rowCells.removeAll(matches);
-    	            		for (Cell rowCell : rowCells) {
-    	            			if (rowCell.remove(i)) {
-    	            				changed = true;
-    	                            Grid.logCandidateRemoval(rowCell, i, getName() + "-C", matches);
-    	            			}
-    	            		}
-    	            	}
-    	            	if (changed) {
-    	                    return true;
-    	                }
-                	}
+            Set<Set<Integer>> combinations = new TreeSet<Set<Integer>>();
+            combinations = Cell.generateCombinations(rowPopulation, 
+                                                     combinations, 
+                                                     getFishSize());
+            
+            for (Set<Integer> combination : combinations) {
+                Set<Map.Entry<Integer, Set<Integer>>> matches = new HashSet<Map.Entry<Integer, Set<Integer>>>();
+                for (Map.Entry<Integer, Set<Integer>> entry : columnMap.entrySet()) {
+                    if (combination.containsAll(entry.getValue())) {
+                        matches.add(entry);
+                    }
+                }
+                if (matches.size() == getFishSize()) {
+                    Set<Cell> fish = new TreeSet<Cell>();
+                    for (Map.Entry<Integer, Set<Integer>> entry : matches) {
+                        House column = columns.get(entry.getKey() - 1);
+                        for (Integer row : entry.getValue()) {
+                            fish.add(column.getCells().get(row - 1));
+                        }
+                        columnMap.remove(entry.getKey());
+                    }
+                    boolean solved = false;
+                    for (Map.Entry<Integer, Set<Integer>> entry : columnMap.entrySet()) {
+                        entry.getValue().retainAll(combination);
+                        House column = columns.get(entry.getKey() - 1);
+                        for (Integer row : entry.getValue()) {
+                            Cell cell = column.getCells().get(row - 1);
+                            cell.remove(i);
+                            Grid.logCandidateRemoval(cell, i, getName(), fish);
+                            solved = true;
+                        }
+                    }
+                    if (solved) {
+                        return true;
+                    }
                 }
             }
         }
         return false;
-    }
-    
-    private Map<Integer, Integer> getFrequencyMap(Set<Integer> keys) {
-    	Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
-    	for (Integer key : keys) {
-    		frequencyMap.put(key, 0);
-    	}
-    	return frequencyMap;
-    }
-    
-    private void incrementFrequencyMap(Map<Integer, Integer> frequencyMap, int key) {
-    	frequencyMap.put(key, frequencyMap.get(key) + 1);
-    }
-    
-    private boolean validateFrequencyMap(Map<Integer, Integer> frequencyMap) {
-    	for (Integer frequency : frequencyMap.values()) {
-    		if (frequency > getFishSize() || frequency < 2) {
-    			return false;
-    		}
-    	}
-    	return true;
     }
 }
