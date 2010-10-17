413e3654f6062d962ea316ab0c9cc7ef96715ceb
davidjgrant1978 <davidjgrant1978@b9633145-1b50-0410-95e4-9528dd1f157e>
2010-10-17 19:45:03 +0000

    Incorrect Maven layout

--- trunk/src/main/test/com/google/code/jsudokusolver/strategy/HiddenSingleTest.java
+++ trunk/src/main/test/com/google/code/jsudokusolver/strategy/HiddenSingleTest.java
@@ -1,10 +1,13 @@
 package com.google.code.jsudokusolver.strategy;
 
+import java.util.ServiceLoader;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.code.jsudokusolver.Grid;
 import com.google.code.jsudokusolver.Solver;
+import com.google.code.jsudokusolver.SolverStrategy;
 
 public class HiddenSingleTest 
 {
@@ -13,9 +16,12 @@ public class HiddenSingleTest
 	{
 		Grid g = Grid.fromString("..2.3...8.....8....31.2.....6..5.27..1.....5.2.4.6..31....8.6.5.......13..531.4..");
 		
-		Solver s = new Solver();
-		s.addStrategy(new Given());
-		s.addStrategy(new HiddenSingle());
+		final Solver s = new Solver();
+		final ServiceLoader<SolverStrategy> strategies = ServiceLoader.load(SolverStrategy.class);
+		for (SolverStrategy strategy : strategies) {
+			System.out.println(strategy);
+			s.addStrategy(strategy);
+		}
 		s.solve(g);
 		
 		Assert.assertEquals("672435198549178362831629547368951274917243856254867931193784625486592713725316489", g.toString());
